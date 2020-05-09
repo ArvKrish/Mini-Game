e236bd8c7f6d7244b6ae263d30fe9f1c52c7d9ee
ArvKrish <64888385+ArvKrish@users.noreply.github.com>
2020-05-09 11:47:30 +0530

    Update Villain.java

--- game/src/game/Villain.java
+++ game/src/game/Villain.java
@@ -2,14 +2,16 @@ package game;
 
 public class Villain extends Character {
 	
-	Villain(String name){
-		super(name,100,20);
+	
+	Villain(String name,int power){
+		super(name,100,power);
 	}
 	 void strikeUpdate() {
 		System.out.println("Does not apply\n Villians can't strike players(for now) :D");
 	}
-	 
-	 void getSummary() {
+	 	 
+	public void getSummary() {
 		 System.out.println(this.toString());
 	 }
-}
+	}
+
